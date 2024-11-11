9443880eb3abca942c78cd7c18d3eee64c3e7b38
muttzilla <muttzilla.music@gmail.com>
2024-11-11 19:19:29 +0000

    Sound Update
    Added music and some SFX, made some changes to the Player script but kept original intact (it has the sound logic but no changes to Will's original stuff) there is a small bug in which the player won't jump until it falls, solved it by loading theplayer a bit above the floor

--- player_2.gd
+++ player_2.gd
@@ -16,8 +16,8 @@ class_name Player
 @onready var coyote_timer: Timer = $Timers/CoyoteTimer
 @onready var buffer_timer: Timer = $Timers/BufferTimer
 @onready var camera: Camera2D = $Camera2D
-@export var activeCamera : bool = true;
-@onready var levelcontroller : Node2D;
+@export var activeCamera : bool = true
+@onready var levelcontroller : Node2D
 
 ## Sound stuff
 @onready var Walk = $Walk
@@ -26,6 +26,8 @@ class_name Player
 @export var step_cooldown = 0.44
 var time_since_last_step = 0.0
 
+# Track if the player was on the ground in the previous frame
+var was_on_floor: bool = true
 
 func _ready():
 	if !activeCamera:
@@ -33,32 +35,35 @@ func _ready():
 		levelcontroller = get_parent()
 
 func _physics_process(delta: float) -> void:
-	if !activeCamera && position.y >= 5:
+	if !activeCamera and position.y >= 5:
 		levelcontroller.reset_level()
-		
+
 	handle_jump()
 	handle_movement(delta)
-	
-	if !is_on_floor() && !coyote_timer.time_left:
+
+	# Updated gravity and jump-related logic
+	if !is_on_floor() and !coyote_timer.time_left:
 		can_jump = false
 		handle_gravity(delta)
-	
-	if movement_direction != 0 && can_move:
+
+	if movement_direction != 0 and can_move:
 		sprite_2d.flip_h = (movement_direction == -1)
-	
-	var was_on_floor : bool = is_on_floor()
-	
+
+	# Move and slide, then check for landing sound and jumping permissions
 	move_and_slide()
-	
-	if !is_on_floor() && was_on_floor:
+
+	# Check if the player has just landed
+	if is_on_floor() and !was_on_floor:
+		if not Fall.playing:
+			Fall.play()
+		can_jump = true  
+	elif !is_on_floor() and was_on_floor:
 		coyote_timer.start()
-	elif is_on_floor() && !was_on_floor:
-		can_jump = false
-		coyote_timer.stop()
-	elif is_on_floor():
-		can_jump = true
 	
-	if Input.is_action_just_pressed("jump") && !is_on_floor():
+	# Update was_on_floor for the next frame
+	was_on_floor = is_on_floor()
+
+	if Input.is_action_just_pressed("jump") and !is_on_floor():
 		buffered_jump = true
 		buffer_timer.start()
 
@@ -68,44 +73,39 @@ func handle_movement(delta: float) -> void:
 	if movement_direction and can_move:
 		velocity.x = move_toward(velocity.x, movement_direction * movement_speed, acceleration * delta)
 		
-		## the time since the last step
+		# Handle footstep sound timing
 		time_since_last_step += delta
-
-		## time passed since the last step
-		if time_since_last_step >= step_cooldown && is_on_floor():
-			if not Walk.playing:
+		if time_since_last_step >= step_cooldown and is_on_floor():
+			if not Walk.playing && not Fall.playing:
 				Walk.play()
 			time_since_last_step = 0.0 
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
-		time_since_last_step = step_cooldown  # Reset timer so it starts fresh when moving resumes
-
+		time_since_last_step = step_cooldown  # Reset timer when movement stops
 
 func handle_jump() -> void:
-	
-	if Input.is_action_just_pressed("jump") \
-	&& ((is_on_floor() || coyote_timer.time_left) \
-	|| is_on_floor() && buffered_jump) && can_move && can_jump:
+	# Jump conditions, now simplified
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote_timer.time_left or buffered_jump) and can_move and can_jump:
 		can_jump = false
+		buffered_jump = false
 		self.velocity.y = jump_force
 		if not Jump.playing:
 			Jump.play()
-	
-	if Input.is_action_just_released("jump") && velocity.y < jump_force / 4 && can_move:
-		can_jump = false
-		self.velocity.y = jump_force / 4
+		buffer_timer.stop()  # Stop buffer timer on successful jump
 
-func handle_gravity(delta : float) -> void:
-	
+	# Short hop when releasing jump button early
+	if Input.is_action_just_released("jump") and velocity.y < jump_force / 4 and can_move:
+		velocity.y = jump_force / 4
+
+func handle_gravity(delta: float) -> void:
+	# Apply gravity based on whether the player is falling or jumping
 	self.velocity.y += get_player_gravity() * delta
-	
+
 func get_player_gravity() -> float:
-	
+	# Different gravity values for jumps and falls
 	if velocity.y < 0:
 		return gravity
 	return fall_gravity
 
-
 func _on_buffer_timer_timeout() -> void:
-	
 	buffered_jump = false
