dd8850354ab3b813b586f1c9ef1a7fb00b872df2
muttzilla <muttzilla.music@gmail.com>
2024-11-12 19:03:46 +0000

    Sound Updates
    When player moves between  worlds music will stop and the portal sound will play, made it so that it changes instantaneously, but might add a lil timer like, 0.3 after sound is played

--- Scenes/Environment/door_reflection.gd
+++ Scenes/Environment/door_reflection.gd
@@ -5,21 +5,26 @@ extends Area2D
 @export var challenge_index : int
 @onready var interactable : bool = false
 
+## Sound stuff
+@onready var MainMusic = $"../Music"
+@onready var PortalOut = $"../PortalOut"
+
 func _ready() -> void:
-	
 	Eventbus.heart_collected.connect(on_heart_collected)
 	self.hide()
 
 func on_heart_collected() -> void:
-	
 	self.show()
 	interactable = true
 
 func _on_area_entered(_area):
-	
 	if !interactable:
 		return
-	
+	MainMusic.stop()
+	PortalOut.play()
+	PortalOut.finished.connect(_on_portal_out_finished)
 	Eventbus.challenge_beaten.emit(challenge_index)
-	await get_tree().create_timer(0.2).timeout
+
+func _on_portal_out_finished() -> void:
+	# Change the scene after the portal sound is done
 	get_tree().change_scene_to_file("res://Scenes/Root/master.tscn")
