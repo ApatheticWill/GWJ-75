3349487536be767a021e7083dab04c4cc5a948a5
ApatheticWill <realgirayaykurt@gmail.com>
2024-11-13 16:19:11 +0300

    minor bugfix

--- Scripts/Root/master.gd
+++ Scripts/Root/master.gd
@@ -4,7 +4,9 @@ class_name Master
 @onready var tutorial_door: LockedDoor = $LockedDoors/TutorialDoor
 @onready var tutorial_mirror: Mirror = $Mirrors/TutorialMirror
 @onready var first_challenge_door: LockedDoor = $LockedDoors/FirstChallengeDoor
+@onready var challenge_mirror_1: Mirror = $Mirrors/ChallengeMirror1
 @onready var second_challenge_door: LockedDoor = $LockedDoors/SecondChallengeDoor
+@onready var challenge_mirror_2: Mirror = $Mirrors/ChallengeMirror2
 @onready var third_challenge_door: LockedDoor = $LockedDoors/ThirdChallengeDoor
 @onready var player: Player = $Player
 
@@ -22,11 +24,11 @@ func check_for_progress() -> void:
 		tutorial_door.call_deferred("queue_free")
 	
 	if GameManager.first_challenge_beaten:
-		# Will Have Mirror.
+		challenge_mirror_1.call_deferred("queue_free")
 		first_challenge_door.call_deferred("queue_free")
 	
 	if GameManager.second_challenge_beaten:
-		# Will Have Mirror.
+		challenge_mirror_2.call_deferred("queue_free")
 		second_challenge_door.call_deferred("queue_free")
 	
 	if GameManager.third_challenge_beaten:
