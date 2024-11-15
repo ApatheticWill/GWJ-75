a67d063ffccedd7f1349467c21a97fc3d8e93ced
ApatheticWill <realgirayaykurt@gmail.com>
2024-11-15 15:16:17 +0300

    add mirror effects

--- Scripts/Environment/mirror.gd
+++ Scripts/Environment/mirror.gd
@@ -4,9 +4,14 @@ class_name Mirror
 @export var scene_resource : MirrorResource
 @export var respawn_point : Vector2
 
+@onready var highlight = preload("res://Assets/Environment/mirror_highlighted.png")
+@onready var default = preload("res://Assets/Environment/mirror_base.png")
 @onready var interactable : bool = false
+@onready var is_interacted : bool = false
 @onready var player : Player = get_tree().get_first_node_in_group("Player")
 @onready var interaction_zone: Area2D = $InteractionZone
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var player_reflection: Sprite2D = $PlayerReflection
 
 ## Sound stuff
 @onready var GameMusic = $"../../Music"  # Background music
@@ -15,16 +20,31 @@ class_name Mirror
 func _ready() -> void:
 	# Connect the `finished` signal of the portal sound to a function that changes the scene
 	PortalIn.finished.connect(_on_portal_sound_finished)
+	player_reflection.hide()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("interact") and interactable:
+		sprite_2d.texture = default
 		player.can_move = false
 		interactable = false
+		is_interacted = true
 		GameMusic.stop()  # Stop the main music
 		PortalIn.play()  # Play the portal sound effect
 
 		# The scene will change once the portal sound finishes (handled by `_on_portal_sound_finished`)
+
+func _physics_process(_delta: float) -> void:
+	
+	if interactable:
+		player_reflection.global_position = player.global_position
+		player_reflection.global_position.x = player.global_position.x - 5
+		player_reflection.flip_h = player.sprite_2d.flip_h
+		player_reflection.show()
+	elif is_interacted && !interactable:
+		player_reflection.show()
+	else:
+		player_reflection.hide()
 
 func _on_portal_sound_finished() -> void:
 	# Set the respawn point and change the scene
@@ -35,8 +55,10 @@ func _on_portal_sound_finished() -> void:
 
 func _on_interaction_zone_body_entered(body: Node2D) -> void:
 	if body is Player:
+		sprite_2d.texture = highlight
 		interactable = true
 
 func _on_interaction_zone_body_exited(body: Node2D) -> void:
 	if body is Player:
+		sprite_2d.texture = default
 		interactable = false
